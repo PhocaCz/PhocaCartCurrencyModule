469c749d25c7dcfc9c2eaa6499829b0e9a10a1ef
Jan <info@phoca.cz>
2019-10-24 14:41:27 +0200

    3.4.0
    3.4.0

--- tmpl/default.php
+++ tmpl/default.php
@@ -69,7 +69,7 @@ echo '<input type="hidden" name="task" value="checkout.currency">';
 echo '<input type="hidden" name="tmpl" value="component" />';
 echo '<input type="hidden" name="option" value="com_phocacart" />';
 echo '<input type="hidden" name="return" value="'.$actionBase64.'" />';
-echo '<div class="'.$this->s['c']['pull-right'].' ph-input-select-currencies-button">';
+echo '<div class="'.$s['c']['pull-right'].' ph-input-select-currencies-button">';
 echo '<button class="btn btn-primary btn-sm ph-btn"><span class="'.$s['i']['refresh'].'"></span> '.JText::_('COM_PHOCACART_CHANGE_CURRENCY').'</button>';
 echo '</div>';
 echo JHtml::_('form.token');
