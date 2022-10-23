25045318be3ac9d21b4966b7769b65768254ef80
n3t <n3t@n3t.cz>
2022-10-24 01:44:29 +0200

    Correct cache usage for different currencies

--- mod_phocacart_currency.php
+++ mod_phocacart_currency.php
@@ -9,6 +9,9 @@
 
 defined('_JEXEC') or die;// no direct access
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ModuleHelper;
+
 /** @var \Joomla\Registry\Registry $params */
 
 if (!JComponentHelper::isEnabled('com_phocacart', true)) {
@@ -50,7 +53,23 @@ $uri 			= \Joomla\CMS\Uri\Uri::getInstance();
 $action			= $uri->toString();
 $actionBase64	= base64_encode($action);
 $linkCheckout	= JRoute::_(PhocacartRoute::getCheckoutRoute());
-$selectBox 		= PhocacartCurrency::getCurrenciesSelectBox($params->get('show_button', true) ? '' : 'onchange="this.form.submit();"');
+
+$session 	= Factory::getApplication()->getSession();
+$activeCurrency	= (int)$session->get('currency', PhocacartCurrency::getDefaultCurrency(), 'phocaCart');
+$cacheid = md5($module->id . '-' . $activeCurrency);
+
+$cacheparams               = new \stdClass();
+$cacheparams->cachemode    = 'id';
+$cacheparams->class        = '\PhocacartCurrency';
+$cacheparams->method       = 'getCurrenciesSelectBox';
+$cacheparams->methodparams = [
+	$params->get('show_button', true) ? '' : 'onchange="this.form.submit();"'
+];
+$cacheparams->modeparams   = $cacheid;
+
+$selectBox     = ModuleHelper::moduleCache($module, $params, $cacheparams);
+//$selectBox 		= PhocacartCurrency::getCurrenciesSelectBox($params->get('show_button', true) ? '' : 'onchange="this.form.submit();"');
+
 $currArray		= PhocacartCurrency::getCurrenciesArray();
 //$selectBox 		= PhocacartCurrency::getCurrenciesArray();
 //$selectBox 		= PhocacartCurrency::getCurrenciesListBox();
